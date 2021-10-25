ba9122e0bde5dd146e77f87b6fa5dfbe38af6883
Lijie Zhou <lijie.zhou@gusto.com>
2021-10-25 16:38:58 -0700

    Showing one recipe

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_recipe(recipe_id):
+    conn = get_db_connection()
+    recipe = conn.execute('SELECT * FROM recipes WHERE id = ?',
+                        (recipe_id,)).fetchone()
+    conn.close()
+    if recipe is None:
+        abort(404)
+    return recipe
 
 app = Flask(__name__)
 
@@ -14,3 +24,8 @@ def index():
     recipes = conn.execute('SELECT * FROM recipes').fetchall()
     conn.close()
     return render_template('index.html', recipes=recipes)
+
+@app.route('/<int:recipe_id>')
+def recipe(recipe_id):
+    recipe = get_recipe(recipe_id)
+    return render_template('recipe.html', recipe=recipe)
